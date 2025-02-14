f3db112091b240fc894e9454b62f721e41359269
Lenur Serveriiev <lenur.serveriev@gmail.com>
2025-02-14 07:08:22 +0100

    Implement MakeDeveloperMap

--- src/main/java/december/tuesday/stream_interview/MakeDeveloperMap.java
+++ src/main/java/december/tuesday/stream_interview/MakeDeveloperMap.java
@@ -1,9 +1,13 @@
 package december.tuesday.stream_interview;
 
-import errors.TaskNotCompleteException;
+import lombok.experimental.UtilityClass;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * The task from real interview on Middle Java Developer position.
@@ -15,6 +19,7 @@ import java.util.Map;
  * 1. Developer::getId not present in Assignment::getDeveloperId - add Developer::getName with Collections::emptyList
  * 2. Assignment::getTaskId not present in Task::getId - Skip Assignment
  */
+@UtilityClass
 public class MakeDeveloperMap {
 
     /**
@@ -27,11 +32,31 @@ public class MakeDeveloperMap {
      * - Key is Developer Name
      * - Value is List of Task Title assigned to Developer Handle.
      */
-    public static Map<String, List<String>> report(
+    public Map<String, List<String>> report(
             List<Task> tasks,
             List<Developer> developers,
-            List<Assignment> assignments) {
-        throw new TaskNotCompleteException();
+            List<Assignment> assignments
+    ) {
+        Map<Integer, String> taskIdToName = new HashMap<>();
+        for (Task task : tasks) {
+            taskIdToName.put(task.id(), task.title());
+        }
+
+        Map<Integer, List<String>> developerToTasks = new HashMap<>();
+        for (Assignment assignment : assignments) {
+            if (taskIdToName.containsKey(assignment.taskId())) {
+                developerToTasks.computeIfAbsent(assignment.developerId(), b -> new ArrayList<>());
+                developerToTasks.get(assignment.developerId()).add(taskIdToName.get(assignment.taskId()));
+            }
+        }
+
+        Map<String, List<String>> res = new HashMap<>();
+
+        for (Developer developer : developers) {
+            res.put(developer.name(), developerToTasks.getOrDefault(developer.id(), Collections.emptyList()));
+        }
+
+        return res;
     }
 
     /**
@@ -43,12 +68,31 @@ public class MakeDeveloperMap {
      * - Key is Developer Name
      * - Value is List of Task Title assigned to Developer Handle.
      */
-    public static Map<String, List<String>> reportWithStreams(
+    public Map<String, List<String>> reportWithStreams(
             List<Task> tasks,
             List<Developer> developers,
-            List<Assignment> assignments) {
+            List<Assignment> assignments
+    ) {
+        Map<Integer, String> taskMap = tasks
+                .stream()
+                .collect(Collectors.toMap(Task::id, Task::title));
 
-        throw new TaskNotCompleteException();
+        Map<Integer, List<String>> developerToTasks = assignments
+                .stream()
+                .filter(a -> taskMap.containsKey(a.taskId()))
+                .collect(Collectors.groupingBy(
+                        Assignment::developerId,
+                        Collectors.mapping(a -> taskMap.get(a.taskId()), Collectors.toList())
+                ));
+
+        return developers
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                Developer::name,
+                                d -> developerToTasks.getOrDefault(d.id(), Collections.emptyList())
+                        )
+                );
     }
 
     // A method to print a map to the console.
